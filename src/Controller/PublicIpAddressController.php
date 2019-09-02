74a52bdb96e7c1e85e8fe815a1e590bf1183b61b
Jeroen de Bruin <jeroen@cupella.nl>
2019-09-02 16:33:30 +0200

    Added `bin/check`

--- src/Controller/PublicIpAddressController.php
+++ src/Controller/PublicIpAddressController.php
@@ -1,12 +1,30 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PublicIpAddressController extends AbstractController
 {
-
+    /**
+     * @Route(
+     *     "/api/ip-address-request/json"
+     * )
+     */
+    public function getPublicIpAddressRequest(
+        Request $request
+    ): Response {
+        return new JsonResponse([
+            'status' => 'OK',
+            'data' => [
+                'ipAddress' => $request->getClientIp(),
+            ],
+        ]);
+    }
 }
